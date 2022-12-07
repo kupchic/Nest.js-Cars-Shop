8118f8bf21a3337ddf11ac139686d5fb4099dc13
Alexander Kupchenia <akupchenia@elinext.com>
2022-12-07 16:04:49 +0300

    03 - review fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import 'dotenv/config';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_HOST, {
-      dbName: 'CarsShop',
+      dbName: process.env.DB_NAME,
     }),
     ManagerModule,
     ProductModule,
