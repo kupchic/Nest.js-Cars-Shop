faabd58cf3aef031968a77ccea21dd2e861d3946
Alexander Kupchenia <akupchenia@elinext.com>
2023-01-03 11:03:13 +0300

    05 - prettier run

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     'prettier/prettier': 'error',
+    'linebreak-style': 0,
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
