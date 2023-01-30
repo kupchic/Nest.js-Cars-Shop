dbdd83a4e05562cacad90942e0236f184afa177f
Alexander Kupchenia <akupchenia@elinext.com>
2023-01-30 11:37:49 +0300

    05 - comments fix

--- src/orders/orders.gateway.ts
+++ src/orders/orders.gateway.ts
@@ -13,6 +13,7 @@ import { ForbiddenException, Logger, UseGuards } from '@nestjs/common';
 import { AuthService } from '../auth/auth.service';
 import { User } from '../user/schemas';
 import { WsJwtGuard } from '../common/guards/ws-jwt.guard';
+import { ExtendedError } from 'socket.io/dist/namespace';
 
 @WebSocketGateway({ namespace: 'orders', transports: ['websocket'] })
 export class OrdersGateway
@@ -25,7 +26,7 @@ export class OrdersGateway
   constructor(private readonly authService: AuthService) {}
 
   afterInit(server: Server): void {
-    server.use(async (socket: Socket, next: any) => {
+    server.use(async (socket: Socket, next: (err?: ExtendedError) => void) => {
       try {
         const authToken: string = (
           socket.handshake.auth.auth_token ||
@@ -48,12 +49,18 @@ export class OrdersGateway
   }
 
   handleConnection(client: Socket): void {
-    const sockets: Map<string, Socket> = this._server.sockets as any;
+    const sockets: Map<string, Socket> = this._server.sockets as unknown as Map<
+      string,
+      Socket
+    >;
     this._logger.log(`WS client with id ${client.id} connected,`);
     this._logger.log(`Number of connected sockets:${sockets.size} `);
   }
   handleDisconnect(client: Socket): void {
-    const sockets: Map<string, Socket> = this._server.sockets as any;
+    const sockets: Map<string, Socket> = this._server.sockets as unknown as Map<
+      string,
+      Socket
+    >;
     this._logger.log(`WS client with id ${client.id} disconnected,`);
     this._logger.log(`Number of connected sockets:${sockets.size} `);
   }
